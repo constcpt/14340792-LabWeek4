8c136cc21c1b4114078120cbba74cbd5e6651f2d
constcpt <51440152+constcpt@users.noreply.github.com>
2023-08-25 16:43:08 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,10 +6,12 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i;
     public Renderer rend;
+    private int randomNum;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        randomNum = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -17,5 +19,13 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log($"{gameObject.name}:{i}");
+        if (gameObject.CompareTag("Red") && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.CompareTag("Blue") && i == randomNum)
+        {
+            rend.enabled = false;
+        }
     }
 }
